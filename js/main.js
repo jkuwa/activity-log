7692b4fcf79ae435b61aca22595ab410d679edf3
kuwa <163106848+jkuwa@users.noreply.github.com>
2025-03-26 12:39:45 +0900

    追加：月間レポート

--- js/main.js
+++ js/main.js
@@ -44,6 +44,8 @@ $(function() {
 
 });
 
+  // グローバル変数
+  let myChart;
 
 {
   // ---------- FullCalendar ----------
@@ -81,4 +83,102 @@ $(function() {
     });
     calendar.render();
   });
+
+
+  // ---------- Chart ----------
+  document.querySelector(".js-selectMonth").addEventListener('click', function() {
+    fetchData();
+  });
+
+  // スプシ取得
+  async function fetchData() {
+    const monthInput = document.querySelector("#month").value;
+    if (!monthInput) {
+      alert("年月を選択してください");
+      return;
+    }
+
+    const [year, month] = monthInput.split("-");
+    const url = `https://script.google.com/macros/s/AKfycby-XNZoasusI39MDGszVTI8PF87MAjr4GOPXZTXQYysFPiALiKL7I1igFvHucNLu9_y/exec?year=${year}&month=${month}`;
+
+    try {
+      const response = await fetch(url);   // 非同期でデータ取得
+      const data = await response.json();   // JSON読み取り
+      const title = `${year}.${month}`;
+
+      updateChart(data, title);
+    } catch (error) {
+      console.error("データ取得エラー: ", error);
+      alert("データ取得に失敗しました");
+    }
+  }
+
+  // グラフの描画
+  function updateChart(data, title) {
+    // weekKeyを取得
+    const allWeekKeys = [...new Set(data.flatMap(item => Object.keys(item).filter(key => key !== 'category')))];
+    allWeekKeys.sort((a, b) => a.localeCompare(b, undefined, {numeric: true}));   // 順に並べる
+    const labels = allWeekKeys;
+    // カテゴリ名取得
+    const categories = data.map(item => item.category);
+
+    // データセット
+    const datasets = categories.map(category => {
+      return {
+        label: category,
+        data: labels.map(weekKey => {
+          const categoryData = data.find(item => item.category === category);
+          return categoryData ? categoryData[weekKey] : 0;
+        }),
+      };
+    });
+
+    const ctx = document.querySelector(".js-chart");
+
+    if (myChart) {
+      myChart.destroy();
+    }
+
+    Chart.defaults.font.family = "'Zen Maru Gothic', 'serif'";
+    myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: datasets
+      },
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: title,
+            position: 'bottom',
+            font: {
+              size: 20,
+              weight: 400
+            },
+            padding: {top: 32}
+          },
+          tooltip: {
+            backgroundColor: '#fff',
+            titleColor: '#246286',
+            titleFont: {
+              size: 14,
+              weight: 400
+            },
+            bodyColor: '#246286',
+          }
+        },
+        scales: {
+          x: {
+            stacked: true
+          },
+          y: {
+            stacked: true,
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
+
